724f882f4f4e1d7f9052f85baaf7c103cc5bd643
kislyakoff <kislyakoff@tut.by>
2022-10-04 12:16:49 +0300

    init commit

---
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplication.java
@@ -0,0 +1,13 @@
+package by.kislyakoff.HomeBudgetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBudgetAppApplication.class, args);
+	}
+
+}
---
+++ HomeBudgetApp/src/test/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kislyakoff.HomeBudgetApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
