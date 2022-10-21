2f5b701df8c478d25a5a8a74cf4189561af7585a
kislyakoff <kislyakoff@tut.by>
2022-10-21 14:14:07 +0300

    add security config and person details model

---
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package by.kislyakoff.HomeBudgetApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import by.kislyakoff.HomeBudgetApp.service.PersonDetailsService;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final PersonDetailsService personDetailsService;
+
+	@Autowired
+	public SecurityConfig(PersonDetailsService personDetailsService) {
+		this.personDetailsService = personDetailsService;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/css/**", "/js/**", "/login*").permitAll()
+			.antMatchers("/homebudget/users/**").hasAuthority("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+				.formLogin()
+					.loginPage("/login")
+					.loginProcessingUrl("/process_login")
+					.defaultSuccessUrl("/homebudget", true)
+					.failureUrl("/login?error")
+			.and() 
+	            .csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(personDetailsService)
+			.passwordEncoder(getPasswordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+
+}
---
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/security/PersonDetails.java
@@ -0,0 +1,65 @@
+package by.kislyakoff.HomeBudgetApp.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import by.kislyakoff.HomeBudgetApp.model.Person;
+
+public class PersonDetails implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final Person person;
+	
+	
+
+	public PersonDetails(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(person.getRole().name()));
+	}
+
+	@Override
+	public String getPassword() {
+		return this.person.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.person.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.person.getIsActive();
+	}
+	
+	public Person getPerson() {
+		return this.person;
+	}
+
+}
