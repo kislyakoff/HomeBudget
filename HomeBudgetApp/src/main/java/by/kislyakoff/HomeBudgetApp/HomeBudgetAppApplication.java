18fde84761743b2bba158bfd88e69ba4ee4678e9
kislyakoff <kislyakoff@tut.by>
2022-10-31 22:07:28 +0300

    add ModelMapper bean to configuration

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplication.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplication.java
@@ -1,7 +1,9 @@
 package by.kislyakoff.HomeBudgetApp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeBudgetAppApplication {
@@ -9,5 +11,11 @@ public class HomeBudgetAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomeBudgetAppApplication.class, args);
 	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
 
 }
