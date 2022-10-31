fba18d83d7f4b63253fec30d9f5361eee1864e0a
kislyakoff <kislyakoff@tut.by>
2022-10-31 13:39:46 +0300

    update CategoriesRepository

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/CategoriesRepository.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/CategoriesRepository.java
@@ -1,6 +1,7 @@
 package by.kislyakoff.HomeBudgetApp.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,12 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import by.kislyakoff.HomeBudgetApp.model.Category;
-import by.kislyakoff.HomeBudgetApp.model.Partner;
 
 @Repository
 public interface CategoriesRepository extends JpaRepository<Category, Integer> {
 	
 	List<Category> findAllByOrderByNameAsc();
+	
+	Optional<Category> findByName(String name);
 	
 	Page<Category> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
