b981ab5f8cd0bc1916cf3c2e609868136e1a9dc9
kislyakoff <kislyakoff@tut.by>
2022-11-30 22:26:00 +0300

    change boolean method for validator
    add List<AccountView> for transaction template

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/AccountsRepository.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/AccountsRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import by.kislyakoff.HomeBudgetApp.dto.projections.AccountView;
 import by.kislyakoff.HomeBudgetApp.model.Account;
 import by.kislyakoff.HomeBudgetApp.model.dict.CurrencyName;
 
@@ -13,5 +14,6 @@ public interface AccountsRepository extends JpaRepository<Account, Integer> {
 	
 	List<Account> findByPersonIdOrderByNameAsc(Integer id);
 	List<Account> findByPersonIdAndActiveTrueOrderByNameAsc(Integer id);
-	boolean existsByNameAndCurrencyCode(String name, CurrencyName currency);
+	boolean existsByNameAndCurrencyCodeAndPersonId(String name, CurrencyName currency, Integer id);
+	List<AccountView> findByPersonIdAndActiveTrue(Integer id);
 }
