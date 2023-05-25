3ace477aeab2436c7bd13616349c57a5e369086a
kislyakoff <kislyakoff@tut.by>
2023-05-25 22:48:43 +0300

    remove model mapper bean

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplication.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/HomeBudgetAppApplication.java
@@ -1,21 +1,13 @@
 package by.kislyakoff.HomeBudgetApp;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeBudgetAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeBudgetAppApplication.class, args);
-	}
-	
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-
+        public static void main(String[] args) {
+                SpringApplication.run(HomeBudgetAppApplication.class, args);
+        }
 
 }
