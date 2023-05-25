241b41a7fe30b9a7b1d771c41d6828897d7dbf1d
kislyakoff <kislyakoff@tut.by>
2023-05-25 22:48:08 +0300

    exception handler for cbrRates requestor

---
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/exceptions/RequesterException.java
@@ -0,0 +1,7 @@
+package by.kislyakoff.HomeBudgetApp.exceptions;
+
+public class RequesterException extends RuntimeException {
+	public RequesterException(Throwable cause) {
+        super(cause);
+    }
+}
