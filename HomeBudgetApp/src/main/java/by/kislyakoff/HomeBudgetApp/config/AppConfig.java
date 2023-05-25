50f444212f7bc57688369cd13f8ae400febbb5cc
kislyakoff <kislyakoff@tut.by>
2023-05-25 22:49:38 +0300

    add some beans for AppConfig class

---
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/config/AppConfig.java
@@ -0,0 +1,45 @@
+package by.kislyakoff.HomeBudgetApp.config;
+
+import java.time.LocalDate;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import by.kislyakoff.HomeBudgetApp.model.CachedCbrCurrencyRates;
+import by.kislyakoff.HomeBudgetApp.util.enumConverters.CurrencyNameConverter;
+import by.kislyakoff.HomeBudgetApp.util.enumConverters.TransactionTypeConverter;
+
+@Configuration
+public class AppConfig {
+
+	private final CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build(true);
+
+	@Bean
+	TransactionTypeConverter transactionTypeConverter() {
+		return new TransactionTypeConverter();
+	}
+
+	@Bean
+	CurrencyNameConverter currencyNameConverter() {
+		return new CurrencyNameConverter();
+	}
+
+	@Bean
+	Cache<LocalDate, CachedCbrCurrencyRates> currencyRateCache(@Value("${app.cache.size}") int cacheSize) {
+		return cacheManager.createCache("CurrencyRate-Cache",
+				CacheConfigurationBuilder.newCacheConfigurationBuilder(LocalDate.class, CachedCbrCurrencyRates.class,
+						ResourcePoolsBuilder.heap(cacheSize)).build());
+	}
+	
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
