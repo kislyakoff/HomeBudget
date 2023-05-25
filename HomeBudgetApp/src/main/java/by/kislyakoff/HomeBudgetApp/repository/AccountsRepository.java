710dbf8ec40009fe8e328f6a5d666e76dfca27f3
kislyakoff <kislyakoff@tut.by>
2023-05-25 14:52:21 +0300

    add balance query

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/AccountsRepository.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/repository/AccountsRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import by.kislyakoff.HomeBudgetApp.dto.projections.AccountView;
+import by.kislyakoff.HomeBudgetApp.dto.projections.BalanceByCurrencyView;
 import by.kislyakoff.HomeBudgetApp.model.Account;
 import by.kislyakoff.HomeBudgetApp.model.dict.CurrencyName;
 
@@ -20,4 +23,8 @@ public interface AccountsRepository extends JpaRepository<Account, Integer> {
 	boolean existsByPersonId(int id);
 	boolean existsByNameAndActiveTrue(String name);
 	Optional<Account> findByIdAndPersonId(int id, int ownerId);
+	
+	@Query("SELECT a.currencyCode AS currencyCode, SUM(a.balance) AS balanceSum "
+			+ "FROM Account a WHERE a.person.id= :personId AND a.includeInTotal = TRUE GROUP BY a.currencyCode")
+	List<BalanceByCurrencyView> getBalanceByPersonId(@Param("personId")int personId);
 }
