9e71819af3ee46d627f7d3a7e68ab25766762c33
kislyakoff <kislyakoff@tut.by>
2023-05-25 22:57:27 +0300

    add fetchtype.lazy for manyToOne

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/model/Transaction.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/model/Transaction.java
@@ -19,120 +19,120 @@ import by.kislyakoff.HomeBudgetApp.model.dict.TransactionType;
 @Entity
 @Table(name = "transaction")
 public class Transaction {
-	
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "transaction_type")
-	@NotNull
-	private TransactionType type;
-	
-	@Column(name = "transaction_date")
-	@NotNull
-	private LocalDate date;
-	
-	@ManyToOne
-	@JoinColumn(name = "acc1_id", referencedColumnName = "id")
-	private Account acc1;
-	
-	@ManyToOne
-	@JoinColumn(name = "acc2_id", referencedColumnName = "id")
-	private Account acc2;
-	
-	@Column(name = "amount")
-	private BigDecimal amount;
-	
-	@ManyToOne
-	@JoinColumn(name = "partner_id", referencedColumnName = "id")
-	private Partner partner;
-	
-	@Column(name = "comment")
-	private String comment;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "category_id", referencedColumnName = "id")
-	private Category category;
 
-	public Long getId() {
-		return id;
-	}
+        @Id
+        @Column(name = "id")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+        @Column(name = "transaction_type")
+        @NotNull
+        private TransactionType type;
 
-	public TransactionType getType() {
-		return type;
-	}
+        @Column(name = "transaction_date")
+        @NotNull
+        private LocalDate date;
 
-	public void setType(TransactionType type) {
-		this.type = type;
-	}
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "acc1_id", referencedColumnName = "id")
+        private Account acc1;
 
-	public LocalDate getDate() {
-		return date;
-	}
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "acc2_id", referencedColumnName = "id")
+        private Account acc2;
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+        @Column(name = "amount")
+        private BigDecimal amount;
 
-	public Account getAcc1() {
-		return acc1;
-	}
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "partner_id", referencedColumnName = "id")
+        private Partner partner;
 
-	public void setAcc1(Account acc1) {
-		this.acc1 = acc1;
-	}
+        @Column(name = "comment")
+        private String comment;
 
-	public Account getAcc2() {
-		return acc2;
-	}
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "category_id", referencedColumnName = "id")
+        private Category category;
 
-	public void setAcc2(Account acc2) {
-		this.acc2 = acc2;
-	}
+        public Long getId() {
+                return id;
+        }
 
-	public BigDecimal getAmount() {
-		return amount;
-	}
+        public void setId(Long id) {
+                this.id = id;
+        }
 
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
+        public TransactionType getType() {
+                return type;
+        }
 
-	public Partner getPartner() {
-		return partner;
-	}
+        public void setType(TransactionType type) {
+                this.type = type;
+        }
 
-	public void setPartner(Partner partner) {
-		this.partner = partner;
-	}
+        public LocalDate getDate() {
+                return date;
+        }
 
-	public String getComment() {
-		return comment;
-	}
+        public void setDate(LocalDate date) {
+                this.date = date;
+        }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+        public Account getAcc1() {
+                return acc1;
+        }
 
-	public Category getCategory() {
-		return category;
-	}
+        public void setAcc1(Account acc1) {
+                this.acc1 = acc1;
+        }
 
-	public void setCategory(Category category) {
-		this.category = category;
-	}
+        public Account getAcc2() {
+                return acc2;
+        }
 
-	@Override
-	public String toString() {
-		return "Transaction [id=" + id + ", type=" + type + ", date=" + date + ", acc1=" + acc1 + ", acc2=" + acc2
-				+ ", amount=" + amount + ", partner=" + partner + ", comment=" + comment + ", category=" + category
-				+ "]";
-	}
-	
+        public void setAcc2(Account acc2) {
+                this.acc2 = acc2;
+        }
+
+        public BigDecimal getAmount() {
+                return amount;
+        }
+
+        public void setAmount(BigDecimal amount) {
+                this.amount = amount;
+        }
+
+        public Partner getPartner() {
+                return partner;
+        }
+
+        public void setPartner(Partner partner) {
+                this.partner = partner;
+        }
+
+        public String getComment() {
+                return comment;
+        }
+
+        public void setComment(String comment) {
+                this.comment = comment;
+        }
+
+        public Category getCategory() {
+                return category;
+        }
+
+        public void setCategory(Category category) {
+                this.category = category;
+        }
+
+        @Override
+        public String toString() {
+                return "Transaction [id=" + id + ", type=" + type + ", date=" + date + ", acc1=" + acc1 + ", acc2=" + acc2
+                                + ", amount=" + amount + ", partner=" + partner + ", comment=" + comment + ", category=" + category
+                                + "]";
+        }
+
 
 }
