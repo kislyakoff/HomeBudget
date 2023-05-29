56d9ecb0fa324108d7d2272bec03a6139da020d7
kislyakoff <kislyakoff@tut.by>
2023-05-29 14:12:55 +0300

    delete extra imports and comments

--- HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/parsers/NbRbCurrencyRateParserJson.java
+++ HomeBudgetApp/src/main/java/by/kislyakoff/HomeBudgetApp/parsers/NbRbCurrencyRateParserJson.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 
 import by.kislyakoff.HomeBudgetApp.model.NbRbCurrencyRate;
 
@@ -19,8 +18,6 @@ public class NbRbCurrencyRateParserJson {
 
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.findAndRegisterModules();
-//		CollectionType typeReference = TypeFactory.defaultInstance().constructCollectionType(List.class,
-//				NbRbCurrencyRate.class);
 		CollectionType typeReference = objectMapper.getTypeFactory().constructCollectionType(List.class,
 				NbRbCurrencyRate.class);
 		List<NbRbCurrencyRate> rates = objectMapper.readValue(ratesAsString, typeReference);
